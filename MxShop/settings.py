ddbe03e8167d4fb4cf03c9fde3f1c4fd502f5ed2
jyw15976 <jiayawu@noahgroup.com>
2020-05-06 11:53:09 +0800

    完成models设计

--- MxShop/settings.py
+++ MxShop/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.users.apps.UsersConfig',
-    'DjangoUeditor',
+    # 'DjangoUeditor',
     'goods',
     'trade',
     'user_operation',
